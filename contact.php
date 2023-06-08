a2e8c571917252dbbcdb0e7aff739629e92f908d
Ozgur Kaya <kayaozgurkaan@gmail.com>
2023-06-08 03:24:57 +0200

    contact us section added our services section improvement better design

--- contact.php
+++ contact.php
@@ -1,24 +1,34 @@
 <?php
+// Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Retrieve form data
   $name = $_POST['name'];
+  $phone = $_POST['phone'];
   $email = $_POST['email'];
+  $package = $_POST['package'];
   $message = $_POST['message'];
 
-  // Validate and sanitize the data
-  // Perform necessary checks on the input values
+  // Validate form data (you can add more validation if needed)
+  if (empty($name) || empty($phone) || empty($email) || empty($package) || empty($message)) {
+    echo "Please fill in all the required fields.";
+    exit;
+  }
 
-  // Process the data (example: send an email)
-  $to = 'your-email@example.com';
-  $subject = 'New contact form submission';
-  $body = "Name: $name\nEmail: $email\n\n$message";
+  // Prepare the email message
+  $to = 'your-email@example.com'; // Replace with your email address
+  $subject = 'Contact Form Submission';
+  $body = "Name: $name\n";
+  $body .= "Phone: $phone\n";
+  $body .= "Email: $email\n";
+  $body .= "Package: $package\n";
+  $body .= "Message: $message\n";
 
-  if (mail($to, $subject, $body)) {
-    // Email sent successfully
-    echo 'Thank you for contacting us. We will get back to you soon.';
+  // Send the email
+  $headers = "From: $name <$email>";
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Thank you for your submission!";
   } else {
-    // Failed to send email
-    echo 'Oops! Something went wrong. Please try again later.';
+    echo "Oops! Something went wrong. Please try again later.";
   }
 }
 ?>
