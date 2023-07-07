9b2e90ec48f4bb315ee23dac387e421f09533b3f
Ozgur Kaya <kayaozgurkaan@gmail.com>
2023-07-07 02:24:59 +0200

    adjusted the team section, make some changes on about-us.html. added services and additional services

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".swiper-container", {
+
+// Hero Slider
+document.addEventListener("DOMContentLoaded", function() {
+  var swiper = new Swiper('#slider-container', {
     slidesPerView: 1,
     spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    loop: true,
     autoplay: {
-      delay: 10000, // Delay in milliseconds (10 seconds)
-      disableOnInteraction: true, // Stop autoplay when user interacts with the slider
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
   });
 });
+
+// FAQ
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+
+  accordionItems.forEach((item) => {
+    const title = item.querySelector(".accordion-title");
+    const content = item.querySelector(".accordion-content");
+    const plusMinusIcon = title.querySelector(".plus-minus");
+
+    title.addEventListener("click", () => {
+      item.classList.toggle("active");
+      content.classList.toggle("hidden");
+      plusMinusIcon.classList.toggle("fa-plus");
+      plusMinusIcon.classList.toggle("fa-minus");
+    });
+  });
+});
+
+// The team
+function toggleOperations(operationsId, detailsId, button) {
+  var operationsDiv = document.getElementById(operationsId);
+  var detailsDiv = document.getElementById(detailsId);
+
+  if (operationsDiv.style.display === "none") {
+    operationsDiv.style.display = "block";
+    detailsDiv.style.display = "none";
+    button.innerText = "See Doctor Details";
+  } else {
+    operationsDiv.style.display = "none";
+    detailsDiv.style.display = "block";
+    button.innerText = "Check Operations";
+  }
+}
