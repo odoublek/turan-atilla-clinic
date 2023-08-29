38a5a0aed7fca9eae91ba5ceba4c58b2885c4785
Ozgur Kaya <kayaozgurkaan@gmail.com>
2023-08-29 23:12:23 +0200

    made latest changes, what we-do testimonials and adjust the latest css. Language switcher is missing

--- contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Process the form data
-    // For demonstration purposes, you can just redirect back to the form page with a query parameter
-    header("Location: contact_form.php?submitted=true");
-    exit;
-}
-?>
--- script.js
+++ script.js
@@ -1,57 +1,21 @@
-// Navbar dropdown menu
-  document.addEventListener("DOMContentLoaded", function () {
-    const surgeriesDropdown = document.querySelector(".group");
-    surgeriesDropdown.addEventListener("mouseover", function () {
-      this.querySelector("ul").classList.remove("hidden");
-    });
-    surgeriesDropdown.addEventListener("mouseout", function () {
-      this.querySelector("ul").classList.add("hidden");
-    });
-  });
-
-
 // Hero Slider
-document.addEventListener("DOMContentLoaded", function() {
-  var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    autoplay: {
-      delay: 300000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    on: {
-      slideChange: function() {
-        var activeSlideIndex = this.realIndex;
-        var heroSection = document.getElementById('hero-section');
-        heroSection.className = '';
-        heroSection.classList.add('slide-' + (activeSlideIndex + 1));
-      },
-    },
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const videoFrame = document.getElementById("video-frame");
+
+  setTimeout(() => {
+    videoFrame.src += "&autoplay=1"; // Start autoplay
+  }, 3000); // Adjust delay time according to your preference
 });
 
-
-
-// navbar hero script
-window.addEventListener('scroll', function() {
-  var contactHeader = document.getElementById('contact-header');
-  var navbar = document.getElementById('navbar');
-  var heroSection = document.getElementById('hero-section');
-
-  if (window.pageYOffset > 0) {
-    contactHeader.classList.remove('connected');
-    navbar.classList.add('navbar-separate');
-    heroSection.classList.add('navbar-separate');
-  } else {
-    contactHeader.classList.add('connected');
-    navbar.classList.remove('navbar-separate');
-    heroSection.classList.remove('navbar-separate');
-  }
+// Navbar dropdown menu
+document.addEventListener("DOMContentLoaded", function () {
+  const surgeriesDropdown = document.querySelector(".group");
+  surgeriesDropdown.addEventListener("mouseover", function () {
+    this.querySelector("ul").classList.remove("hidden");
+  });
+  surgeriesDropdown.addEventListener("mouseout", function () {
+    this.querySelector("ul").classList.add("hidden");
+  });
 });
 
 // FAQ
@@ -71,51 +35,59 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+// Language switcher
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    { pageLanguage: "en" },
+    "google_translate_element"
+  );
+}
 
+// Load Google Translate script
+function loadGoogleTranslateScript(selectedLanguage) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src =
+    "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+  document.head.appendChild(script);
+}
 
-//Testimonial
+// mobile settings
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  mobileMenuToggle.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
+
+// Testimonial
 document.addEventListener("DOMContentLoaded", function () {
   const testimonialContainers = document.querySelectorAll(".testimonial-container");
   const totalTestimonials = testimonialContainers.length;
 
+  // Initialize the first two testimonials as visible
+  for (let i = 0; i < 2; i++) {
+    testimonialContainers[i].classList.add("visible");
+  }
+
   function showNextTestimonials() {
-    const currentContainer = testimonialContainers[currentTestimonialIndex];
-    const nextIndex = (currentTestimonialIndex + 1) % totalTestimonials;
-    const nextContainer = testimonialContainers[nextIndex];
+    const visibleContainers = document.querySelectorAll(".testimonial-container.visible");
 
-    currentContainer.classList.remove("visible");
-    nextContainer.classList.add("visible");
+    for (const container of visibleContainers) {
+      container.classList.remove("visible");
+    }
 
-    currentTestimonialIndex = nextIndex;
+    for (let i = 0; i < 2; i++) {
+      const nextIndex = (currentTestimonialIndex + i + 1) % totalTestimonials;
+      const nextContainer = testimonialContainers[nextIndex];
+      nextContainer.classList.add("visible");
+    }
+
+    currentTestimonialIndex = (currentTestimonialIndex + 1) % totalTestimonials;
   }
 
   let currentTestimonialIndex = 0;
-  testimonialContainers[currentTestimonialIndex].classList.add("visible");
-
   setInterval(showNextTestimonials, 3000);
 });
-
-// Packages
-const packages = document.querySelectorAll('.package');
-
-
-packages.forEach(package => {
-  package.addEventListener('mouseenter', () => {
-    package.querySelector('.package-card').style.transform = 'rotateY(180deg)';
-  });
-
-  package.addEventListener('mouseleave', () => {
-    package.querySelector('.package-card').style.transform = 'rotateY(0)';
-  });
-});
-
-
-//contact form
-  const contactForm = document.querySelector("#contact-form");
-  const successMessage = document.querySelector("#success-message");
-
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    contactForm.classList.add("hidden");
-    successMessage.classList.remove("hidden");
-  });
